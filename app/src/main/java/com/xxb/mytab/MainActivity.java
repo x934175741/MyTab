b51ae93d0f6acd2fc8074767d31dba37677a766a
934175741 <934175741@qq.com>
2017-09-14 11:28:22 +0800

    第一次提交

---
+++ app/src/main/java/com/xxb/mytab/MainActivity.java
@@ -0,0 +1,20 @@
+package com.xxb.mytab;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.longsh.longshlibrary.PagerSlidingTabStrip;
+
+public class MainActivity extends AppCompatActivity {
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pager = (ViewPager) findViewById(R.id.pager);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+    }
+}
